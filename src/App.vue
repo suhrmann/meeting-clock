70a97b55fcc0e4eba8d56b69aff01bd65ffd26c5
Simon Uhrmann <simon.uhrmann@th-deg.de>
2025-02-11 15:39:59 +0100

    Extract AppBar / navigation to own component

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,7 @@
 <template>
-  <v-app id="inspire">
-    <v-app-bar
-      class="px-3"
-      density="compact"
-      flat
-    >
-      <v-avatar
-        class="hidden-md-and-up"
-        color="grey-darken-1"
-        size="32"
-      ></v-avatar>
-
-      <v-spacer></v-spacer>
-
-      <v-tabs
-        align-tabs="center"
-        color="grey-darken-2"
-      >
-        <v-tab
-          v-for="link in links"
-          :key="link"
-          :text="link"
-        ></v-tab>
-      </v-tabs>
-      <v-spacer></v-spacer>
-
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey-darken-1"
-        size="32"
-      ></v-avatar>
-    </v-app-bar>
+  <v-app id="meeting-clock">
+    <!-- Navigation bar-->
+    <AppNavBar />
 
     <v-main class="bg-grey-lighten-3">
       <v-container>
@@ -55,25 +26,3 @@
     </v-main>
   </v-app>
 </template>
-
-<script setup>
-  const links = [
-    'Dashboard',
-    'Messages',
-    'Profile',
-    'Updates',
-  ]
-</script>
-
-<script>
-  export default {
-    data: () => ({
-      links: [
-        'Dashboard',
-        'Messages',
-        'Profile',
-        'Updates',
-      ],
-    }),
-  }
-</script>
---
+++ src/components/AppNavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app-bar class="px-3" density="compact" flat>
+        <v-avatar class="hidden-md-and-up" color="grey-darken-1" size="32"></v-avatar>
+
+        <v-spacer></v-spacer>
+
+        <v-tabs align-tabs="center" color="grey-darken-2">
+            <v-tab v-for="link in links" :key="link" :text="link"></v-tab>
+        </v-tabs>
+        <v-spacer></v-spacer>
+
+        <v-avatar class="hidden-sm-and-down" color="grey-darken-1" size="32"></v-avatar>
+    </v-app-bar>
+</template>
+
+<script setup>
+const links = [
+    'Dashboard',
+    'Messages',
+    'Profile',
+    'Updates',
+]
+</script>
