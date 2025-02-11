5d186503c8ccd3f76d9838686146023e0c1082de
Simon Uhrmann <simon.uhrmann@th-deg.de>
2025-02-11 19:59:48 +0100

    Split up salary tables into data and Pinia store

---
+++ src/stores/salaryTables.js
@@ -0,0 +1,54 @@
+/*
+ * Salary tables.
+ */
+export const monthlySalaryTables = {
+  /**
+   * Monatsentgelte TV-L 2025 für Stufe 1
+   */
+  monthlySalariesTVL: {
+    'title': 'Entgelttabelle TV-L 2025',
+    'source': 'https://oeffentlicher-dienst.info/c/t/rechner/tv-l/allg?id=tv-l-2025&matrix=1',
+    'entgelte': {
+      'E1 (Stufe 2)': 2434.49,
+      'E2 (Stufe 1)': 2642.84,
+      'E3 (Stufe 1)': 2815.57,
+      'E4 (Stufe 1)': 2849.24,
+      'E5 (Stufe 1)': 2973.97,
+      'E6 (Stufe 1)': 3135.83,
+      'E7 (Stufe 1)': 3135.83,
+      'E8 (Stufe 1)': 3319.52,
+      'E9a (Stufe 1)': 3520.10,
+      'E9b (Stufe 1)': 3520.10,
+      'E10 (Stufe 1)': 3928.42,
+      'E11 (Stufe 1)': 4064.54,
+      'E12 (Stufe 1)': 4193.48,
+      'E13 (Stufe 1)': 4629.74,
+      'E14 (Stufe 1)': 5003.49,
+      'E15 (Stufe 1)': 5504.26,
+    },
+  },
+
+  /**
+   * Beamtenbesoldung Bayern 2025 - Besoldungsordnung A
+   */
+  monthlySalariesBeamteBY_A: {
+    'title': 'Beamtenbesoldung Bayern 2025 - Besoldungsordnung A (erste Stufe)',
+    'source': 'https://oeffentlicher-dienst.info/c/t/rechner/tv-l/allg?id=tv-l-2025&matrix=1',
+    'entgelte': {
+      'A3 (Stufe 1)': 2784.00,
+      'A4 (Stufe 2)': 2853.40,
+      'A5 (Stufe 2)': 2889.32,
+      'A6 (Stufe 2)': 2960.92,
+      'A7 (Stufe 2)': 3074.21,
+      'A8 (Stufe 2)': 3150.76,
+      'A9 (Stufe 2)': 3294.99,
+      'A10 (Stufe 2)': 3537.12,
+      'A11 (Stufe 3)': 4045.29,
+      'A12 (Stufe 4)': 4527.30,
+      'A13 (Stufe 5)': 5247.58,
+      'A14 (Stufe 5)': 5612.74,
+      'A15 (Stufe 6)': 6445.94,
+      'A16 (Stufe 6)': 7104.54
+    }
+  }
+}
