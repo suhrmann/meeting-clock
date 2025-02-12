c280a97b61095b760aa4b9b610b7bc271a14ef6b
Simon Uhrmann <simon.uhrmann@th-deg.de>
2025-02-12 14:07:23 +0100

    Add more salary tables (from Oeffentlicher-Dienst.info)

--- src/stores/salaryTables.js
+++ src/stores/salaryTables.js
@@ -1,5 +1,6 @@
 /*
  * Salary tables.
+ * Source: https://oeffentlicher-dienst.info
  */
 export const monthlySalaryTables = {
   /**
@@ -9,22 +10,22 @@ export const monthlySalaryTables = {
     'title': 'Entgelttabelle TV-L 2025',
     'source': 'https://oeffentlicher-dienst.info/c/t/rechner/tv-l/allg?id=tv-l-2025&matrix=1',
     'entgelte': {
-      'E1 (Stufe 2)': 2434.49,
-      'E2 (Stufe 1)': 2642.84,
-      'E3 (Stufe 1)': 2815.57,
-      'E4 (Stufe 1)': 2849.24,
-      'E5 (Stufe 1)': 2973.97,
-      'E6 (Stufe 1)': 3135.83,
-      'E7 (Stufe 1)': 3135.83,
-      'E8 (Stufe 1)': 3319.52,
-      'E9a (Stufe 1)': 3520.10,
-      'E9b (Stufe 1)': 3520.10,
-      'E10 (Stufe 1)': 3928.42,
-      'E11 (Stufe 1)': 4064.54,
-      'E12 (Stufe 1)': 4193.48,
-      'E13 (Stufe 1)': 4629.74,
-      'E14 (Stufe 1)': 5003.49,
-      'E15 (Stufe 1)': 5504.26,
+      'E 1 (Stufe 2)': 2434.49,
+      'E 2 (Stufe 1)': 2642.84,
+      'E 3 (Stufe 1)': 2815.57,
+      'E 4 (Stufe 1)': 2849.24,
+      'E 5 (Stufe 1)': 2973.97,
+      'E 6 (Stufe 1)': 3135.83,
+      'E 7 (Stufe 1)': 3135.83,
+      'E 8 (Stufe 1)': 3319.52,
+      'E 9a (Stufe 1)': 3520.10,
+      'E 9b (Stufe 1)': 3520.10,
+      'E 10 (Stufe 1)': 3928.42,
+      'E 11 (Stufe 1)': 4064.54,
+      'E 12 (Stufe 1)': 4193.48,
+      'E 13 (Stufe 1)': 4629.74,
+      'E 14 (Stufe 1)': 5003.49,
+      'E 15 (Stufe 1)': 5504.26,
     },
   },
 
@@ -35,20 +36,83 @@ export const monthlySalaryTables = {
     'title': 'Beamtenbesoldung Bayern 2025 - Besoldungsordnung A (erste Stufe)',
     'source': 'https://oeffentlicher-dienst.info/c/t/rechner/tv-l/allg?id=tv-l-2025&matrix=1',
     'entgelte': {
-      'A3 (Stufe 1)': 2784.00,
-      'A4 (Stufe 2)': 2853.40,
-      'A5 (Stufe 2)': 2889.32,
-      'A6 (Stufe 2)': 2960.92,
-      'A7 (Stufe 2)': 3074.21,
-      'A8 (Stufe 2)': 3150.76,
-      'A9 (Stufe 2)': 3294.99,
-      'A10 (Stufe 2)': 3537.12,
-      'A11 (Stufe 3)': 4045.29,
-      'A12 (Stufe 4)': 4527.30,
-      'A13 (Stufe 5)': 5247.58,
-      'A14 (Stufe 5)': 5612.74,
-      'A15 (Stufe 6)': 6445.94,
-      'A16 (Stufe 6)': 7104.54
+      'A 3 (Stufe 1)': 2784.00,
+      'A 4 (Stufe 2)': 2853.40,
+      'A 5 (Stufe 2)': 2889.32,
+      'A 6 (Stufe 2)': 2960.92,
+      'A 7 (Stufe 2)': 3074.21,
+      'A 8 (Stufe 2)': 3150.76,
+      'A 9 (Stufe 2)': 3294.99,
+      'A 10 (Stufe 2)': 3537.12,
+      'A 11 (Stufe 3)': 4045.29,
+      'A 12 (Stufe 4)': 4527.30,
+      'A 13 (Stufe 5)': 5247.58,
+      'A 14 (Stufe 5)': 5612.74,
+      'A 15 (Stufe 6)': 6445.94,
+      'A 16 (Stufe 6)': 7104.54
     }
-  }
+  },
+
+  monthlySalariesBeamteBY_B: {
+    'title': 'Beamtenbesoldung Bayern 2025 - Besoldungsordnung B',
+    'source': 'https://oeffentlicher-dienst.info/c/t/rechner/tv-l/allg?id=tv-l-2025&matrix=1',
+    'entgelte': {
+      'B 2':  8860.51,
+      'B 3':  9369.71,
+      'B 4':  9903.06,
+      'B 5':  10515.00,
+      'B 6':  11092.82,
+      'B 7':  11654.94,
+      'B 8':  12240.77,
+      'B 9':  12968.22,
+      'B 10': 15227.10,
+      'B 11': 15809.30,
+    }
+  },
+
+  monthlySalariesBeamteBY_W: {
+    'title': 'Beamtenbesoldung Bayern 2025 - Besoldungsordnung W',
+    'source': 'https://oeffentlicher-dienst.info/c/t/rechner/tv-l/allg?id=tv-l-2025&matrix=1',
+    'entgelte': {
+      'W 1 (Stufe 0)': 5538.75,
+      'W 2 (Stufe 1)': 6824.23,
+      'W 3 (Stufe 1)': 8038.92,
+    }
+  },
+
+  monthlySalariesBeamteBY_R: {
+    'title': 'Beamtenbesoldung Bayern 2025 - Besoldungsordnung R',
+    'source': 'https://oeffentlicher-dienst.info/c/t/rechner/tv-l/allg?id=tv-l-2025&matrix=1',
+    'entgelte': {
+      'R 1 (Stufe 2)': 5253.69,
+      'R 2 (Stufe 4)': 6521.41,
+      'R 3 (Stufe 0)': 9369.71,
+      'R 4 (Stufe 0)': 9903.06,
+      'R 5 (Stufe 0)': 10515.00,
+      'R 6 (Stufe 0)': 11092.82,
+      'R 7 (Stufe 0)': 11654.94,
+      'R 8 (Stufe 0)': 12240.77,
+      'R 9 (Stufe 0)': 12968.22,
+    }
+  },
+
+  monthlySalariesBeamteBY_AW: {
+    'title': 'Beamtenbesoldung Bayern 2025 - Besoldungsordnung AW',
+    'source': 'https://oeffentlicher-dienst.info/c/t/rechner/tv-l/allg?id=tv-l-2025&matrix=1',
+    'entgelte': {
+      'AW A 3': 1389.33,
+      'AW A 4': 1389.33,
+      'AW A 5': 1509.93,
+      'AW A 6': 1509.93,
+      'AW A 7': 1509.93,
+      'AW A 8': 1509.93,
+      'AW A 9': 1563.85,
+      'AW A 10':  1563.85,
+      'AW A 11':  1563.85,
+      'AW A 12':  1703.44,
+      'AW A 13':  1735.21,
+      'AW A 13Z': 1770.08,
+      'AW R 1': 1770.08,
+    }
+  },
 }
