1b834576fcbcfe261444fab2e03b6ec1b6d54310
Simon Uhrmann <simon.uhrmann@th-deg.de>
2025-02-13 17:21:26 +0100

    Fix GH Pages deployment: Add base url to Vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,6 +13,10 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  // fix base url for GitHub Pages
+  base: '/meeting-clock/',
+
   plugins: [
     VueRouter(),
     Layouts(),
