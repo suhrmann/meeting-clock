23121814b0eda8e3921015475cf08eaffa3e7592
Simon Uhrmann <simon.uhrmann@th-deg.de>
2025-02-14 20:47:39 +0100

    Remove settings column completely

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,25 +16,5 @@
 
       </v-sheet>
     </v-col>
-
-    <!-- Config column -->
-    <v-col
-      v-if="!is_production /* only show in dev */"
-      cols="12"
-      md="3"
-    >
-      <v-sheet
-        min-height="268"
-        rounded="lg"
-      >
-        <!-- Settings options -->
-        <MeetingClockSettings />
-
-      </v-sheet>
-    </v-col>
   </v-row>
 </template>
-
-<script setup>
-const is_production = import.meta.env.PROD;
-</script>
